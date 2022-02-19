2593fb4d95e539d3d62f8df25f15e9bb872307fb
Tomek Tkaczyk <tomasz.tkaczyk@unipromax.pl>
2022-02-19 21:05:25 +0100

    Add a register page

--- server/src/server.js
+++ server/src/server.js
@@ -6,12 +6,18 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-app.use(morgan("combine"));
+app.use(morgan("combined"));
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/status", (req, res) => {
   res.send({ message: "hello world !!!" });
+});
+
+app.post("/register", (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email} Your user was registered! Have fun !!!`,
+  });
 });
 
 app.listen(process.env.PORT || 8081);
