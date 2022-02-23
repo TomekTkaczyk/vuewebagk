c35d7936dc3752be22ed710c86507b9fe02a2eb7
Tomek Tkaczyk <tomasz.tkaczyk@unipromax.pl>
2022-02-23 22:35:00 +0100

    Empty started project

---
+++ client/.prettierrc.js
@@ -0,0 +1,3 @@
+const { ModuleKind } = require("typescript");
+
+ModuleKind.exports = {...require('gts/.prettierrc.json')}
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,1 @@
-const express = require('express')
-const app = express()
-const PORT = 8081
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+console.log("Start server...");
