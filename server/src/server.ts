a7040adcdb999ab931886f11db282c5f25e177fa
Tomek Tkaczyk <tomasz.tkaczyk@unipromax.pl>
2022-03-01 21:04:57 +0100

    Impement database migration

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+createConnection().then(async (connection) => {
+    connection.runMigrations();
+    console.log("Applying migration database...");
+    app.listen(8081, () => {
+        console.log("Server listen at http://localhost:8081...");
+    });
+    // console.log("Inserting a new user into the database...");
+    // const user = new User();
+    // // await connection.manager.save(user);
+    // await user.save();
+    // console.log("Saved a new user with id: " + user.Id);
+
+    // console.log("Loading users from the database...");
+    // const users = await connection.manager.find(User);
+    // console.log("Loaded users: ", users);
+
+    // console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
